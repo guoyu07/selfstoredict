cc4c8082b72845c89ba55d5d0bc483d57611b2a8
markus schulte <ms@dom.de>
2017-10-30 22:56:05 +0100

    changing setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='selfstoredict',
-    version='0.4',
+    version='0.5',
     packages=[''],
     url='https://github.com/markus61/selfstoredict',
     license='MIT',
     author='markus',
     author_email='ms@dom.de',
-    description='a python class delivering a dict that stores itself into a JSON file'
+    description='a python class delivering a dict that stores itself into a JSON file or a redis db'
 )
