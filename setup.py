c770fd0dd4976e66299f51f71a71ad9c1875d699
markus schulte <ms@dom.de>
2017-12-08 11:23:09 +0100

    version 0.6  - move from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-from distutils.core import setup
-#from setuptools import setup
-
+from setuptools import setup, find_packages
 setup(
     name='selfstoredict',
-    version='0.5',
-    packages=['selfstoredict'],
+    version='0.6',
+    packages=find_packages(),
     url='https://github.com/markus61/selfstoredict',
     license='MIT',
     author='markus',
     author_email='ms@dom.de',
-    description='a python class delivering a dict that stores itself into a JSON file or a redis db'
+    description='a python class delivering a dict that stores itself into a JSON file or a redis db',
 )
